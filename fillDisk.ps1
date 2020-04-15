e26784d6d245d594b21570abd669f80bbd2ded00
Lubor PETR <lubor.petr@thalesgroup.com>
2020-04-15 14:37:32 +0200

    Change destination filename

--- fillDisk.ps1
+++ fillDisk.ps1
@@ -1,3 +1,3 @@
 # Use all remaining free space on C partition
 
-[io.file]::Create("$env:temp\reallybigfile-deleteme.txt").SetLength((gwmi Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace).Close
+[io.file]::Create("$env:temp\chaos-disk-fill-experiment.txt").SetLength((gwmi Win32_LogicalDisk -Filter "DeviceID='C:'").FreeSpace).Close
